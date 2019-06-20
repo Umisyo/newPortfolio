9c80537e588ac5d9a55b3bd374548b0044f6bbe8
Umisyo <kusunokisouta@gmail.com>
2019-06-20 16:54:57 +0900

    Add:Cardコンポーネント(WIP)

---
+++ components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <h1>{{card_title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card_title: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 50%;
+  height: auto;
+  background-color: white;
+  box-shadow: 2px;
+}
+</style>
